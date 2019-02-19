74314059b932dd579c6ccf1e2f1208241454460b
Matt Coppola <matt.coppola@gmail.com>
2019-02-18 21:42:39 -0600

    added comments to index.js

--- public/client.js
+++ public/client.js
@@ -1,20 +1,26 @@
 let OUTS = 0;
 let baseHit = false;
 
+// runs pitcher function
 $('.pitch').on('click', function(e) {
     pitcher();
 });
 
+// resets inning after 3 outs
 $('.reset-inning').on('click', function(e) {
     console.log("Resetting Outs to Zero")
     OUTS = 0;
 });
 
+// dice roll alert - you can probably remove this at some point
 $('.dice-roll').on('click', function(e) {
     let roll = diceRoll();
     alert(`You rolled a ${roll}.`);
 })
 
+// pitcher function, keeps track of strikes and balls
+// will call hit function if a hit occurs
+// will call umpire function when strikeout or walk occurs
 function pitcher() {
     let strikes = 0;
     let balls = 0;
@@ -53,6 +59,7 @@ function pitcher() {
     }
 }
 
+// random diceroller function
 function diceRoll() {
     let roll = 0
     roll = Math.floor(Math.random() * 6) + 1;
@@ -60,6 +67,7 @@ function diceRoll() {
     return roll;
 }
 
+// umpire checks for balls and strikes and will call accordingly
 function umpire(balls, strikes) {
     if (balls === 4) {
         console.log("Walk!");
@@ -71,6 +79,7 @@ function umpire(balls, strikes) {
     console.log("Umpire says total Outs: ", OUTS);
 }
 
+// hit function carries out a hit and let's batter know the power of the hit
 function hit() {
     let count = 0;
     let dice = diceRoll();
